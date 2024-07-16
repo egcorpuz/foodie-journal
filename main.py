7f6434ea6b826cb6c2e8f26224a8d05ba126c847
Gian <earlgian.corpuz@gmail.com>
2024-07-16 16:01:16 +0800

    clean TODO, use env variables, and create Procfile

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
-# TODO: ✅ Import your forms from the forms.py
 from forms import RegisterForm, CreatePostForm, LoginForm, CommentForm
 from dotenv import load_dotenv
 import smtplib
 import os
 
-# TODO: ✅ decorator function, similar to login_required, that gives admin access only
+load_dotenv()
+
 def admin_only(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
@@ -26,11 +26,10 @@ def admin_only(func):
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
-# TODO: ✅ Initialize Gravatar class Create the Gravatar object
 gravatar = Gravatar(app,
                     size=100,
                     rating='g',
@@ -40,7 +39,6 @@ gravatar = Gravatar(app,
                     use_ssl=False,
                     base_url=None)
 
-# TODO: ✅ Configure Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -54,14 +52,13 @@ def load_user(user_id):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 
 # CONFIGURE TABLES
 # with one-to-many biderectional relationship
-# TODO: ✅ Create a User table for all your registered users.
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -92,7 +89,6 @@ class BlogPost(db.Model):
     
     blog_comments = relationship("Comment", back_populates="parent_post")
     
-# TODO: ✅ Create the comment table
 class Comment(db.Model):
     __tablename__ = "comments"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -110,7 +106,6 @@ with app.app_context():
     db.create_all()
 
 
-# TODO: ✅ Use Werkzeug to hash the user's password when creating a new user.
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     register_form = RegisterForm()
@@ -142,7 +137,6 @@ def register():
     return render_template("register.html", form=register_form)
 
 
-# TODO: ✅ Retrieve a user from the database based on their email.
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
@@ -176,7 +170,6 @@ def get_all_posts():
     return render_template("index.html", all_posts=posts)
 
 
-# TODO: ✅ Allow logged-in users to comment on posts
 @app.route("/post/<int:post_id>", methods=["GET", "POST"])
 def show_post(post_id):
     requested_post = db.get_or_404(BlogPost, post_id)
@@ -196,7 +189,6 @@ def show_post(post_id):
     return render_template("post.html", post=requested_post, form=comment_form)
 
 
-# TODO: ✅ Use a decorator so only an admin user can create a new post
 @app.route("/new-post", methods=["GET", "POST"])
 @admin_only
 def add_new_post():
@@ -216,7 +208,6 @@ def add_new_post():
     return render_template("make-post.html", form=form)
 
 
-# TODO: ✅ Use a decorator so only an admin user can edit a post
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
 @admin_only
 def edit_post(post_id):
@@ -239,7 +230,6 @@ def edit_post(post_id):
     return render_template("make-post.html", form=edit_form, is_edit=True)
 
 
-# TODO: ✅ Use a decorator so only an admin user can delete a post
 @app.route("/delete/<int:post_id>")
 @admin_only
 def delete_post(post_id):
@@ -257,7 +247,6 @@ def about():
 # DON'T put your email and password here directly! The code will be visible when you upload to Github.
 # Use environment variables instead (Day 35)
 
-load_dotenv()
 MAIL_ADDRESS = os.environ.get("EMAIL_KEY")
 MAIL_APP_PW = os.environ.get("PASSWORD_KEY")
 
@@ -271,7 +260,7 @@ def contact():
 
 
 def send_email(name, email, phone, message):
-    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    email_message = f"Subject:New Message from JournalWebApp\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
     with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
         connection.starttls()
         connection.login(MAIL_ADDRESS, MAIL_APP_PW)
